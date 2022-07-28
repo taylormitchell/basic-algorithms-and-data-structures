4c2fbe88e1b9f3b8e9a883f74c9c5634fc19132c
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-07-28 13:02:56 -0400

    Wrap hashtable tests in single describe

--- hashTable.test.js
+++ hashTable.test.js
@@ -2,52 +2,52 @@
 const { HashTable } = require('./hashTable.doing');
 
 
-describe("set", () => {
-    test("should set a value", () => {
-        let hashTable = new HashTable() 
-        hashTable.set('a', 1)
-        expect(hashTable.get('a')).toEqual(1)
+describe("HashTable", () => {
+    describe("set", () => {
+        test("should set a value", () => {
+            let hashTable = new HashTable() 
+            hashTable.set('a', 1)
+            expect(hashTable.get('a')).toEqual(1)
+        })
+        test("should set many values", () => {
+            let hashTable = new HashTable()
+            hashTable.set('a', 1)
+            hashTable.set('b', 2)
+            hashTable.set('c', 3)
+            expect(hashTable.get('a')).toEqual(1)
+            expect(hashTable.get('b')).toEqual(2)
+            expect(hashTable.get('c')).toEqual(3)
+        })
+        test("should overwrite a value", () => {
+            let hashTable = new HashTable() 
+            hashTable.set('a', 1)
+            hashTable.set('a', 2)
+            expect(hashTable.get('a')).toEqual(2)
+        })
     })
-    test("should set many values", () => {
-        let hashTable = new HashTable()
-        hashTable.set('a', 1)
-        hashTable.set('b', 2)
-        hashTable.set('c', 3)
-        expect(hashTable.get('a')).toEqual(1)
-        expect(hashTable.get('b')).toEqual(2)
-        expect(hashTable.get('c')).toEqual(3)
+
+    describe("get", () => {
+        test("should get a value", () => {
+            let hashTable = new HashTable() 
+            hashTable.set('a', 1)
+            expect(hashTable.get('a')).toEqual(1)
+        })
+        test("should return null when missing a value", () => {
+            let hashTable = new HashTable()
+            expect(hashTable.get('a')).toEqual(null)
+        })
     })
-    test("should overwrite a value", () => {
-        let hashTable = new HashTable() 
-        hashTable.set('a', 1)
-        hashTable.set('a', 2)
-        expect(hashTable.get('a')).toEqual(2)
-    })
+
+    describe("delete", () => {
+        test("should delete a value", () => {
+            let hashTable = new HashTable() 
+            hashTable.set('a', 1)
+            hashTable.delete('a')
+            expect(hashTable.get('a')).toEqual(null)
+        })
+        test("should return false when missing a value", () => {
+            let hashTable = new HashTable()
+            expect(hashTable.delete('a')).toEqual(false)
+        })
+    })   
 })
-
-describe("get", () => {
-    test("should get a value", () => {
-        let hashTable = new HashTable() 
-        hashTable.set('a', 1)
-        expect(hashTable.get('a')).toEqual(1)
-    })
-    test("should return null when missing a value", () => {
-        let hashTable = new HashTable()
-        expect(hashTable.get('a')).toEqual(null)
-    })
-})
-
-describe("delete", () => {
-    test("should delete a value", () => {
-        let hashTable = new HashTable() 
-        hashTable.set('a', 1)
-        hashTable.delete('a')
-        expect(hashTable.get('a')).toEqual(null)
-    })
-    test("should return false when missing a value", () => {
-        let hashTable = new HashTable()
-        expect(hashTable.delete('a')).toEqual(false)
-    })
-})   
-
-

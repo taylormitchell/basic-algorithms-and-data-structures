b8ca47b65e2a02301b23f96c0d97b26f2acdf208
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-08-18 08:04:26 -0400

    Add linkedList.todo.js

---
+++ linkedList.todo.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor() {}
+}
+
+class LinkedList {
+  constructor(...nodes) {}
+
+  push = (node) => {};
+
+  delete = (node) => {};
+
+  insert = (node, nodeInsertAt) => {};
+
+  search = (value) => {};
+
+  values = () => {
+    let result = [];
+    let node = this.head;
+    while (node) {
+      result.push(node.value);
+      node = node.next;
+    }
+    return result;
+  };
+}
+
+module.exports = { LinkedList, Node };

04935005ff48dc65d7baf8a6db995a13ef242b48
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-09-23 08:34:49 -0400

    Update linkedList tests

--- linkedList.test.js
+++ linkedList.test.js
@@ -1,60 +1,65 @@
-// const { LinkedList } = require('./linkedList');
-const { LinkedList } = require("./linkedList.doing");
+let LinkedList;
 
-test("constructor", () => {
-  let ll = new LinkedList();
-  expect(ll.head).toEqual(null);
+describe.each(["./linkedList.todo", "./linkedList"])("%s", (filename) => {
+  beforeAll(() => {
+    LinkedList = require(filename).LinkedList;
+  });
 
-  ll = new LinkedList(1, 2, 3);
-  expect(ll.values()).toEqual([1, 2, 3]);
-});
+  test("constructor", () => {
+    let ll = new LinkedList();
+    expect(ll.head).toEqual(null);
 
-test("insert", () => {
-  let values = [1, 3, 4];
-  let ll = new LinkedList(...values);
-  ll.insert(0, 0);
-  expect(ll.values()).toEqual([0, 1, 3, 4]);
-  ll.insert(2, 2);
-  expect(ll.values()).toEqual([0, 1, 2, 3, 4]);
-});
+    ll = new LinkedList(1, 2, 3);
+    expect(ll.values()).toEqual([1, 2, 3]);
+  });
 
-test("push", () => {
-  let ll = new LinkedList(1, 2, 3);
-  ll.push(0);
-  expect(ll.values()).toEqual([0, 1, 2, 3]);
-});
+  test("insert", () => {
+    let values = [1, 3, 4];
+    let ll = new LinkedList(...values);
+    ll.insert(0, 0);
+    expect(ll.values()).toEqual([0, 1, 3, 4]);
+    ll.insert(2, 2);
+    expect(ll.values()).toEqual([0, 1, 2, 3, 4]);
+  });
 
-test("delete", () => {
-  let ll = new LinkedList(1, 2, 3, 4);
+  test("push", () => {
+    let ll = new LinkedList(1, 2, 3);
+    ll.push(0);
+    expect(ll.values()).toEqual([0, 1, 2, 3]);
+  });
 
-  // delete head
-  ll.delete(1);
-  expect(ll.values()).toEqual([2, 3, 4]);
+  test("delete", () => {
+    let ll = new LinkedList(1, 2, 3, 4);
 
-  // delete middle
-  ll.delete(3);
-  expect(ll.values()).toEqual([2, 4]);
+    // delete head
+    ll.delete(1);
+    expect(ll.values()).toEqual([2, 3, 4]);
 
-  // delete tail
-  ll.delete(4);
-  expect(ll.values()).toEqual([2]);
+    // delete middle
+    ll.delete(3);
+    expect(ll.values()).toEqual([2, 4]);
 
-  // try deleting non-contained node
-  expect(ll.delete(0)).toEqual(false);
-  expect(ll.values()).toEqual([2]);
-});
+    // delete tail
+    ll.delete(4);
+    expect(ll.values()).toEqual([2]);
 
-test("delete from empty", () => {
-  let ll = new LinkedList();
-  expect(ll.delete(0)).toEqual(false);
-});
+    // try deleting non-contained node
+    expect(ll.delete(0)).toEqual(false);
+    expect(ll.values()).toEqual([2]);
+  });
 
-test("has", () => {
-  let ll = new LinkedList();
-  expect(ll.has(2)).toEqual(false);
-  ll = new LinkedList(1, 2, 3);
-  expect(ll.has(1)).toEqual(true);
-  expect(ll.has(2)).toEqual(true);
-  expect(ll.has(3)).toEqual(true);
-  expect(ll.has(4)).toEqual(false);
+  test("delete from empty", () => {
+    let ll = new LinkedList();
+    expect(ll.delete(0)).toEqual(false);
+  });
+
+  test("has", () => {
+    let ll = new LinkedList();
+    expect(ll.has(2)).toEqual(false);
+    ll = new LinkedList(1, 2, 3);
+    expect(ll.has(1)).toEqual(true);
+    expect(ll.has(2)).toEqual(true);
+    expect(ll.has(3)).toEqual(true);
+    expect(ll.has(4)).toEqual(false);
+  });
 });

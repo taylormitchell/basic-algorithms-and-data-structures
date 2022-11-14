58478c703d9e3af3626724b4756a15b843ffb8b5
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-11-14 08:46:55 -0500

    Remove unused scripts

--- scripts/do.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const todoFile = process.argv[2];
-const doingFile = todoFile.replace("todo", "doing");
-fs.copyFile(todoFile, doingFile, (err) => {
-  if (err) throw err;
-});

f863f79310447d0d09f7459ddb1ef07257763e24
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-02-13 21:33:06 -0500

    update

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -3,11 +3,11 @@ export function binarySearch(arr, value, start = 0, end = arr.length) {
     return null;
   }
   const mid = Math.floor((start + end) / 2);
-  if (arr[mid] === value) {
+  if (value === arr[mid]) {
     return mid;
-  } else if (arr[mid] < value) {
-    return binarySearch(arr, value, mid + 1, end);
-  } else {
+  } else if (value < arr[mid]) {
     return binarySearch(arr, value, start, mid);
+  } else {
+    return binarySearch(arr, value, mid + 1, end);
   }
 }

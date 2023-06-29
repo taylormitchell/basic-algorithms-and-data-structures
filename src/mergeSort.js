c71e421bd28c2ef9e184d032c80650db179c1c20
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-06-29 08:34:52 -0400

    Clean up mergeSort

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,23 +1,23 @@
 export function mergeSort(arr) {
   if (arr.length < 2) return arr;
-  var middle = Math.floor(arr.length / 2);
-  var left = arr.slice(0, middle);
-  var right = arr.slice(middle);
+  var mid = Math.floor(arr.length / 2);
+  var left = arr.slice(0, mid);
+  var right = arr.slice(mid);
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
   const result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
+  let l = 0;
+  let r = 0;
+  while (l < left.length && r < right.length) {
+    if (left[l] < right[r]) {
+      result.push(left[l]);
+      l++;
     } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
+      result.push(right[r]);
+      r++;
     }
   }
-  return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+  return [...result, ...left.slice(l), ...right.slice(r)];
 }
